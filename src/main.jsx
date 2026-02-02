c4d607a08fe6059ad093ea6c599e879298605669
Lui Lavayen <lu14393@gmail.com>
2026-02-02 19:20:09 -0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
-import AdminPanel from './pages/Admin.jsx';
 import MediaDashboard from './pages/MediaDashboard.jsx';
 import PhotoUpload from './pages/PhotoUpload.jsx';
 import PhotoGallery from './pages/PhotoGallery.jsx';
@@ -13,14 +12,9 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/admin" element={<AdminPanel />} />
-        <Route path="/admin/mediacenter" element={<MediaDashboard />} />
-        <Route path="/admin/mediacenter/upload" element={<PhotoUpload />} />
-        <Route path="/admin/mediacenter/gallery" element={<PhotoGallery />} />
-        {/* Public Routes for Guests */}
-        <Route path="/mediacenter" element={<MediaDashboard />} />
-        <Route path="/mediacenter/upload" element={<PhotoUpload />} />
-        <Route path="/mediacenter/gallery" element={<PhotoGallery />} />
+        <Route path="/media" element={<MediaDashboard />} />
+        <Route path="/media/upload" element={<PhotoUpload />} />
+        <Route path="/media/gallery" element={<PhotoGallery />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
